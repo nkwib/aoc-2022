b751b41e12274916d64c214b33c70cb25b0288b8
Gabriele Magno <gabriele.magno@immerse.io>
2022-12-02 10:22:16 +0100

    created submodules for js and go

--- js/index.js
+++
@@ -1,4 +0,0 @@
-import {first, second} from './day1/first.js';
-
-console.log("The elf with the most calories is: ", first());
-console.log("The sum of the elves with the most calories is: ", second());
--- js/utils.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-const __dirname = path.resolve();
-
-export const readFile = (day, puzzle) => {
-  return fs.readFileSync(`${__dirname}/day${day}/input_${puzzle}.txt`, 'utf8');
-}
